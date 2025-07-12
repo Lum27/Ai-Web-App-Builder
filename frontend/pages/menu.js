f63e5a77784fad9644c012b55ed4a91c7255b960
TyR <123214704+Lum27@users.noreply.github.com>
2025-07-12 16:12:47 -0400

    Create menu.js

---
+++ frontend/pages/menu.js
@@ -0,0 +1,12 @@
+export default function Menu() {
+  return (
+    <div>
+      <h1>Client Portal</h1>
+      <ul>
+        <li><a href="/dashboard">Live Preview</a></li>
+        <li><a href="/index">Manual Prompt Builder</a></li>
+        <li><a href="/frozen">Freeze/Unfreeze</a></li>
+      </ul>
+    </div>
+  );
+}
